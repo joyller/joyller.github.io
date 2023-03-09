094de180ea0530987c07bf7ace6835095d54bfa0
joyller <106361452+joyller@users.noreply.github.com>
2023-03-08 23:17:26 -0400

    Add files via upload

--- Scripts/Validador.js
+++ Scripts/Validador.js
@@ -1,60 +1,42 @@
-function prueba(cedu){
-	var MiCedula = parseInt(cedu,10);
-	
-	var digitos = MiCedula.toString();
-	var separar = digitos.split('');
-	var digReal = separar.map(Number);
+function prueba(cedu) {
+
+	let ejemplo = document.getElementById("cedula").value;
+
+	var cedula = ejemplo.split('');
 	var total = 0;
-	var multipar = 0;
-	var verificador = 0;
+	var auxiliar = 0;
 
-for (var x = 0; x < 10 ; x++){
-	if((x+1)%2 != 0)
-	{
-		total= (total + digReal[x])
-	}
+	var digReal = 0;
 
-	if((x+1)%2 == 0)
-	{
-		multiPar = digReal[x] * 2;
-		if(multiPar >= 10)
-		{
-			var aux = multiPar.toString();
-			var auxT = aux.split('');
-			var auxN = auxT.map(Number);
-			
-			total = (total + auxN[0]+auxN[1]);
-		}else 
-		{
-			total = (total + multiPar)
+	for (var x = 0; x < 10; x++) {
+		if ((x + 1) % 2 != 0) {
+			total = parseInt(Number(total) + Number(cedula[x]));
+		}
+
+		if ((x + 1) % 2 == 0) {
+
+			auxiliar = parseInt(Number(cedula[x]) * Number(2));
+
+			if (auxiliar < 10) {
+				total = parseInt(Number(total) + Number(auxiliar));
+			} else if (auxiliar >= 10) {
+				var aux = String(auxiliar).split("").map((auxiliar) => {
+					return Number(auxiliar)
+				})
+
+				auxiliar = parseInt(Number(aux[0]) + Number(aux[1]));
+
+				total = parseInt(Number(total) + Number(auxiliar));
+			}
 		}
 	}
-}
 
-verificador = (total % 10);
+	digReal = parseInt(Number(10) - (Number(total) % Number(10)));
 
-if(verificador > 0)
-{
-	verificador = 10-verificador;
-	if(verificador == digReal[10])
-	{
+	if (digReal == cedula[10] || digReal == 10) {
 		document.getElementById("valido").innerHTML = '<b style="color:rgb(167, 219, 131);">cedula valida</b>';
-	}
-	else
-	{
-		document.getElementById("valido").innerHTML = '<b style="color:rgb(219, 144, 131);">cedula invalida</b>';
-	}
-
-}else
-{
-	if( 0 == digReal[10])
-	{
-		document.getElementById("valido").innerHTML = '<b style="color:rgb(167, 219, 131);">cedula valida</b>';
-	}
-	else
-	{
+	} else {
 		document.getElementById("valido").innerHTML = '<b style="color:rgb(219, 144, 131);">cedula invalida</b>';
 	}
 }
 
-} // ???	
