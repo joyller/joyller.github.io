c0194c00098a0dea5c6b9e488c7494cdbcfd061d
joyller <106361452+joyller@users.noreply.github.com>
2023-03-27 10:03:50 -0400

    Update Script_Menu.js

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -1,4 +1,4 @@
-var alm_menu = fetch('https://joyller.github.io/datafile/Menu.json').then((response) => response.json()).then((json) => console.log(json));
+var almmenu = fetch('https://joyller.github.io/datafile/Menu.json').then((response) => response.json()).then((json) => console.log(json));
 
 console.log("hola");
-console.log(alm_menu);
+console.log(almmenu);
