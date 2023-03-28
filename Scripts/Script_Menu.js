1401ea6f0c5ea2049a80a29b6870d3bd9dd185bc
joyller <106361452+joyller@users.noreply.github.com>
2023-03-28 10:32:33 -0400

    Update Script_Menu.js

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -6,3 +6,32 @@ console.log(almmenu);
 function crearMenu(){
   
 }*/
+
+$(function () {
+
+    var data = fetch('https://joyller.github.io/datafile/Menu.json').then((response) => response.json());
+    var getMenuItem = function (itemData) {
+        var item = $("<li>")
+            .append(
+        $("<a>", {
+            href: '#' + itemData.link,
+            html: itemData.name
+        }));
+        if (itemData.sub) {
+            var subList = $("<ul>");
+            $.each(itemData.sub, function () {
+                subList.append(getMenuItem(this));
+            });
+            item.append(subList);
+        }
+        return item;
+    };
+    
+    var $menu = $("#menu");
+    $.each(data.menu, function () {
+        $menu.append(
+            getMenuItem(this)
+        );
+    });
+    $menu.menu();
+});
