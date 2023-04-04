f0b1dd0abf43f0f20e8632f1199ec1e660913cab
joyller <106361452+joyller@users.noreply.github.com>
2023-04-04 19:25:55 -0400

    prueba 4

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -1,22 +1,20 @@
-function prueba() {
-    fetch('https://joyller.github.io/datafile/Menu.json')
-            .then(response => response.json())
-            .then(data => {
-               // Get the menu container
-               const menu = document.getElementById('menu');
+fetch('https://joyller.github.io/datafile/Menu.json')
+    .then(response => response.json())
+    .then(data => {
+        // Get the menu container
+        const menu = document.getElementById('menu');
 
-               // Create the menu items
-               data.menu.forEach(item => {
-                  const link = document.createElement('a');
-                  link.href = item.url;
-                  link.textContent = item.name;
+        // Create the menu items
+        data.menu.forEach(item => {
+            const link = document.createElement('a');
+            link.href = item.url;
+            link.textContent = item.name;
 
-                  const listItem = document.createElement('li');
-                  listItem.appendChild(link);
+            const listItem = document.createElement('li');
+            listItem.appendChild(link);
 
-                  menu.appendChild(listItem);
-               });
-            })
-            .catch(error => console.error(error));
-}
-         // Load the JSON file
+            menu.appendChild(listItem);
+        });
+    })
+    .catch(error => console.error(error));
+
