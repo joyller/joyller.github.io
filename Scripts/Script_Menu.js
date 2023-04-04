acf7b4341dff272697b4961be9772a3ce50bb6c9
joyller <106361452+joyller@users.noreply.github.com>
2023-04-04 18:24:09 -0400

    cambio 1

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -1,8 +1,15 @@
 function prueba(){
     console.log("hola");
 
+    var arreglo = new XMLHttpRequest();
+    arreglo.open("GET",  'https://joyller.github.io/datafile/Menu.json', false);
+    arreglo.send(null);
+    var jsonObject = JSON.parse(arreglo.responseText);
+
     for (var x = 3 ; x < 10 ; x++)
     {
-        console.log(x);
+        console.log(jsonObject);
+
+        
     }
 }
