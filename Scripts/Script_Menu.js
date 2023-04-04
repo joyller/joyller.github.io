a9913cffc4fa140ee6afc7a17da04063e1116068
joyller <106361452+joyller@users.noreply.github.com>
2023-04-04 18:47:33 -0400

    cmabio 4

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -6,8 +6,12 @@ function prueba(){
     arreglo.send(null);
     var jsonObject = JSON.parse(arreglo.responseText);
 
-    for (var x = 3 ; x < 10 ; x++)
+    for (var x = 0 ; x < jsonObject.lenght ; x++)
     {
-        console.log(jsonObject.ID[x]);   
+        console.log(jsonObject[0]);   
+
+        var fila =  '<tr> <td>jsonObject[x].ID</td> <td>jsonObject[x].Desc</td> <td>jsonObject[x].Tipo</td></tr>';
+
+        table.innerHTML += fila;
     }
 }
