8a48e2f6a05599adb20a871d7bc7d742d30a285b
joyller <106361452+joyller@users.noreply.github.com>
2023-04-04 21:14:05 -0400

    tal

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -1,10 +1,9 @@
+
 fetch('https://joyller.github.io/datafile/Menu.json')
     .then(response => response.json())
     .then(data => {
-        // Get the menu container
         const menu = document.getElementById('menu');
 
-        // Create the menu items
         data.menu.forEach(item => {
             const link = document.createElement('a');
             link.href = item.url;
@@ -13,8 +12,27 @@ fetch('https://joyller.github.io/datafile/Menu.json')
             const listItem = document.createElement('li');
             listItem.appendChild(link);
 
+            // Check if the menu item has a submenu
+            if (item.subMenu) {
+                const subMenu = document.createElement('ul');
+
+                // Create the submenu items
+                item.subMenu.forEach(subItem => {
+                    const subLink = document.createElement('a');
+                    subLink.href = subItem.url;
+                    subLink.textContent = subItem.name;
+
+                    const subListItem = document.createElement('li');
+                    subListItem.appendChild(subLink);
+
+                    subMenu.appendChild(subListItem);
+                });
+
+                // Add the submenu to the menu item
+                listItem.appendChild(subMenu);
+            }
+
             menu.appendChild(listItem);
         });
     })
     .catch(error => console.error(error));
-
