6d5e556a5916774b61a43d1c06caed0cc90d0ebe
joyller <106361452+joyller@users.noreply.github.com>
2023-04-05 09:26:20 -0400

    Update Script_Menu.js

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -1,38 +1,19 @@
-
 fetch('https://joyller.github.io/datafile/Menu.json')
-    .then(response => response.json())
-    .then(data => {
-        const menu = document.getElementById('menu');
+  .then(response => response.json())
+  .then(data => {
+    const menu = document.getElementById('menu');
 
-        data.menu.forEach(item => {
-            const link = document.createElement('a');
-            link.href = item.url;
-            link.textContent = item.name;
+    data.menu.forEach(item => {
+      const link = document.createElement('a');
+      link.href = item.ident;
+      link.textContent = item.desc;
 
-            const listItem = document.createElement('li');
-            listItem.appendChild(link);
-
-            // Check if the menu item has a submenu
-            if (item.subMenu) {
-                const subMenu = document.createElement('ul');
-
-                // Create the submenu items
-                item.subMenu.forEach(subItem => {
-                    const subLink = document.createElement('a');
-                    subLink.href = subItem.url;
-                    subLink.textContent = subItem.name;
-
-                    const subListItem = document.createElement('li');
-                    subListItem.appendChild(subLink);
-
-                    subMenu.appendChild(subListItem);
-                });
-
-                // Add the submenu to the menu item
-                listItem.appendChild(subMenu);
-            }
-
-            menu.appendChild(listItem);
-        });
-    })
-    .catch(error => console.error(error));
+      const listItem = document.createElement('li');
+      listItem.appendChild(link);
+    
+        console.log(menu.ident[item]);
+        console.log(menu.desc[item]);
+      menu.appendChild(listItem);
+    });
+  })
+  .catch(error => console.error(error));
