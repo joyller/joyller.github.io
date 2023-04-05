33fe67191f1f78edc10afcf823805c1c3f005d99
joyller <106361452+joyller@users.noreply.github.com>
2023-04-05 10:19:54 -0400

    Update Script_Menu.js

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -1,7 +1,11 @@
-async function menu() {
-  const response = await fetch('https://joyller.github.io/datafile/Menu.json');
-  const ident = await response.json();
-  console.log(ident); 
-  // logs [{ name: 'Joker'}, { name: 'Batman' }]
-}
-menu();
+fetch('example.json')
+  .then(response => response.json())
+  .then(data => {
+    for (const key in data) {
+      if (data.hasOwnProperty(key)) {
+        const value = data[key];
+        console.log(key + ': ' + value);
+      }
+    }
+  })
+  .catch(error => console.error(error));
