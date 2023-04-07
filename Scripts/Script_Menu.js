1af545fc0266f2f8ab0f05fc33955599a13a8255
joyller <106361452+joyller@users.noreply.github.com>
2023-04-07 12:18:36 -0400

    tomate

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -1,10 +1,11 @@
 fetch('https://joyller.github.io/datafile/Menu.json')
-.then(response => response.json())
-.then(data => {
-  // Find the person with name "Jane Smith"
-  const jane = data.find(person => person.name === "Jane Smith");
-  
-  // Access Jane's age using dot notation or bracket notation
-  console.log(jane.age);
-})
-.catch(error => console.error(error));
+  .then(response => response.json())
+  .then(Menu => {
+    // Loop through each person in the array
+    Menu.forEach(Option => {
+      // Access each person's data using dot notation or bracket notation
+      console.log(Option.father);
+      console.log(Option.ident);
+    });
+  })
+  .catch(error => console.error(error));
