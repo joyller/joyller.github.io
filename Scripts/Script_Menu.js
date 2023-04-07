6f5c56a5dbf614757cfa87d780f1734ac29124c2
joyller <106361452+joyller@users.noreply.github.com>
2023-04-07 15:40:13 -0400

    asda

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -4,24 +4,22 @@ fetch('https://joyller.github.io/datafile/Menu.json')
 
     let list = document.getElementById("El_Menu");
 
-    var fix = -1;
     // Loop through each person in the array
     Menu.forEach(Opcion => {
-
-      while(!!~(fix = Opcion.findIndex(e => Opcion[i].pid == e.id)) && fix > i) [Opcion[i],a[fix]] = [Opcion[fix],Opcion[i]];
-      lut[Opcion[i].id]=i;
+      
       // Access each person's data using dot notation or bracket notation
-      console.log(Opcion.padre);
+      //console.log(Opcion.padre);
       
       let li = document.createElement("li");
       li.innerText = Opcion.descripcion;
       list.appendChild(li);
 
-      console.log(Opcion.identificador);
+      if(Opcion.padre != null)
+      {
+        console.log(Opcion.descripcion)
+      }
+      //console.log(Opcion.identificador);
 
     });
   })
   .catch(error => console.error(error));
-
-
-  
