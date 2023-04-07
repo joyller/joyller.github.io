14893676821eafcddacc6ba9438dc169373210e8
joyller <106361452+joyller@users.noreply.github.com>
2023-04-07 16:13:19 -0400

    asd

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -4,20 +4,18 @@ fetch('https://joyller.github.io/datafile/Menu.json')
 
     let list = document.getElementById("El_Menu");
 
-    // Loop through each person in the array
     Menu.forEach(Opcion => {
-      
-      // Access each person's data using dot notation or bracket notation
       //console.log(Opcion.padre);
       
-      let li = document.createElement("li");
-      li.innerText = Opcion.descripcion;
-      list.appendChild(li);
-
       if(Opcion.padre != null)
       {
         console.log(Opcion.descripcion)
       }
+
+      let lista = document.createElement("li");
+      lista.innerText = Opcion.descripcion;
+      list.appendChild(lista);
+
       //console.log(Opcion.identificador);
 
     });
