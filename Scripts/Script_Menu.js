bc854cad427bf01d446f6a115c8cfe41a2c42c52
joyller <106361452+joyller@users.noreply.github.com>
2023-04-07 12:40:48 -0400

    preba 1

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -1,11 +1,20 @@
 fetch('https://joyller.github.io/datafile/Menu.json')
   .then(response => response.json())
   .then(Menu => {
+
+    let list = document.getElementById("El_Menu");
+
     // Loop through each person in the array
-    Menu.forEach(Option => {
+    Menu.forEach(Opcion => {
       // Access each person's data using dot notation or bracket notation
-      console.log(Option.father);
-      console.log(Option.ident);
+      console.log(Opcion.padre);
+      
+      let li = document.createElement("li");
+      li.innerText = Opcion.descripcion;
+      list.appendChild(li);
+
+      console.log(Opcion.identificador);
+
     });
   })
   .catch(error => console.error(error));
