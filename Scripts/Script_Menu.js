de57d5ed7aef2e0b22810a89a28e1d74f8b8350c
joyller <106361452+joyller@users.noreply.github.com>
2023-04-08 19:53:50 -0400

    asssssssssssssssssss

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -2,10 +2,15 @@ fetch('https://joyller.github.io/datafile/Menu.json')
   .then(response => response.json())
   .then(Menu => {
         
+
+
     Menu.forEach(Father => {
       Menu.forEach(Hijo => {
         if(Father.identificador == Hijo.padre){
           console.log (Father.descripcion + " es el padre de " + Hijo.descripcion); 
+          console.log(Padre.padre);
+          console.log(Padre.descripcion);
+          console.log(Padre.identificador);
         }
       });
     });
