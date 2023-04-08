2809ff78c18f20be19fb2f249df972095e9b2c14
joyller <106361452+joyller@users.noreply.github.com>
2023-04-08 17:46:07 -0400

    txt

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -2,7 +2,18 @@ fetch('https://joyller.github.io/datafile/Menu.json')
   .then(response => response.json())
   .then(Menu => {
 
-    Menu.forEach(Opcion => {
+    let lista = document.createElement("li");
+
+    for (var pa = 0; pa < Menu.length ; pa++){
+      for(var hi = 0; hi < Menu.length ; hi++){
+        if(Menu[pa].padre == Menu[hi].identificador){
+          lista.innerText = Opcion.descripcion;
+          list.appendChild(lista)
+        }
+      }
+    }
+
+    /*Menu.forEach(Opcion => {
       console.log(Opcion.padre);
       console.log(Opcion.descripcion);
       console.log(Opcion.identificador);
@@ -15,10 +26,9 @@ fetch('https://joyller.github.io/datafile/Menu.json')
       enlance.setAttribute('href', link);
       El_Menu.appendChild(enlance);
 
-      let lista = document.createElement("li");
       lista.innerText = Opcion.descripcion;
-      list.appendChild(lista);*/
+      list.appendChild(lista);
 
-    });
+    });*/
   })
   .catch(error => console.error(error))
