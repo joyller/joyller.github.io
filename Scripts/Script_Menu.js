f02141be4bec2ede7de976af5c5c8e518e7c53e6
joyller <106361452+joyller@users.noreply.github.com>
2023-04-08 19:48:42 -0400

    xzc

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -1,26 +1,26 @@
 fetch('https://joyller.github.io/datafile/Menu.json')
   .then(response => response.json())
   .then(Menu => {
+    
     let lista = document.getElementById("Menuhtml");
-
+    
     Menu.forEach(Father => {
       let li = document.createElement("li");
       li.innerText = Father.descripcion;
       lista.appendChild(li);
 
-      /*Menu.forEach(Hijo => {
+      Menu.forEach(Hijo => {
         if(Father.identificador == Hijo.padre){
-          console.log (Father.descripcion + " es el padre de " + Hijo.descripcion);
-          
+          console.log (Father.descripcion + " es el padre de " + Hijo.descripcion); 
 
         }
         
         /*console.log(Padre.padre);
         console.log(Padre.descripcion);
-        console.log(Padre.identificador);
+        console.log(Padre.identificador);*/
         
 
-      });*/
+      });
     });
   })
   .catch(error => console.error(error))
