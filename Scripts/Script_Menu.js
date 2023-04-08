7d1cf16754a0a0c3e6f92bf9a4e8de505c7b480d
joyller <106361452+joyller@users.noreply.github.com>
2023-04-07 22:20:31 -0400

    meh

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -4,34 +4,27 @@ fetch('https://joyller.github.io/datafile/Menu.json')
 
     let list = document.getElementById("El_Menu");
 
-    let respaldo = Menu;
-    
-    respaldo.forEach(respaldo => {
-        Menu.sort((a, b) => a.identificador - b.padre);
-    });
-
-    for(var t = 0 ; t < Menu.length ; x++){
-      console.log(Menu[x].descripcion);
-    }
-    /*Menu.forEach(Opcion => {
-      //console.log(Opcion.padre);
+    Menu.forEach(Opcion => {
+      console.log(Opcion.padre);
 
       if (Opcion.padre != null) {
-        console.log(Opcion.descripcion)
+        console.log(Opcion.descripcion);
+
+        let link = Opcion.url;
+        let enlance = document.createElement('a');
+        enlance.innerHTML = Opcion.descripcion;
+        enlance.setAttribute('href', link);
+        El_Menu.appendChild(enlance);
+
+        if (Opcion.padre !=null) {
+          console.log(Menu[x].descripcion);
+          let lista = document.createElement("li");
+          lista.innerText = Opcion.descripcion;
+          list.appendChild(lista);
+          console.log(Opcion.identificador);
+        }
       }
-      let link = Opcion.url;
-      let enlance = document.createElement('a');
-      enlance.innerHTML = Opcion.descripcion;
-      enlance.setAttribute('href', link);
-      El_Menu.appendChild(enlance);
 
-      //if(Opcion.padre == Menu[])
-      //console.log(Menu[x].descripcion);
-      //let lista = document.createElement("li");
-      //lista.innerText = Opcion.descripcion;
-      //list.appendChild(lista);
-      //console.log(Opcion.identificador);
-
-    });*/
+      });
   })
   .catch(error => console.error(error))
