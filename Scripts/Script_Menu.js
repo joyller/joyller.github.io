e73d87250bdfd7d3f3aea6dd7ae3cb229633ef91
joyller <106361452+joyller@users.noreply.github.com>
2023-04-07 23:13:23 -0400

    a

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -2,14 +2,32 @@ fetch('https://joyller.github.io/datafile/Menu.json')
   .then(response => response.json())
   .then(Menu => {
 
-    let list = document.getElementById("El_Menu");
+    let list = document.createElement("ul");
+
+    function crearmenu(Menu, list){
+      for(x = 0; x < Menu.length ; x++){
+        objmenu = Menu[x];
+        lista = document.createElement("li");
+        if(objmenu.padre === null){
+          enlace = document.createElement("a");
+          enlace.innerHTML = objmenu.descripcion;
+          enlace.href = objmenu.url;
+          li.appendChild(enlace);
+          crearmenu.appendChild(li);
+        }
+      }
+
+      return crearmenu;
+    }
 
     Menu.forEach(Opcion => {
       console.log(Opcion.padre);
       console.log(Opcion.descripcion);
       console.log(Opcion.identificador);
 
-      let link = Opcion.url;
+
+
+      /*let link = Opcion.url;
       let enlance = document.createElement('a');
       enlance.innerHTML = Opcion.descripcion;
       enlance.setAttribute('href', link);
@@ -17,8 +35,13 @@ fetch('https://joyller.github.io/datafile/Menu.json')
 
       let lista = document.createElement("li");
       lista.innerText = Opcion.descripcion;
-      list.appendChild(lista);
+      list.appendChild(lista);*/
 
     });
   })
   .catch(error => console.error(error))
+
+  crearmenu = nestItems(objmenu, crearmenu)
+
+  let listanavegacion = document.querySelector("#listanavegacion");
+  listanavegacion.appendChild(crearmenu);
