0d0a972c40886e7e98edb0733c09041cc8dabefd
joyller <106361452+joyller@users.noreply.github.com>
2023-04-08 21:41:10 -0400

    zx

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -13,8 +13,7 @@ fetch('https://joyller.github.io/datafile/Menu.json')
       Menu.forEach(Hijo => {
         if(Father.identificador == Hijo.padre){
           console.log (Father.descripcion + " es el padre de " + Hijo.descripcion); 
-          li.innerText = Hijo.descripcion;
-          lista.appendChild(li);
+    
         }
       });
     });
