4ad1926b58cbded944905d902e714a32c4100250
joyller <106361452+joyller@users.noreply.github.com>
2023-04-09 20:18:35 -0400

    prueba

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -1,45 +1,50 @@
+// cargar el JSON
 fetch('https://joyller.github.io/datafile/Menu.json')
   .then(response => response.json())
   .then(Menu => {
+    // Almacena el menu 
+    // llama la funcion 
+    const lista = CrearMenu(Menu);
 
+    // Prueba de datos
     Menu.forEach(Opcion => {
-        console.log(Opcion.padre);
-        console.log(Opcion.descripcion);
-        console.log(Opcion.identificador);
+      console.log(Opcion.padre);
+      console.log(Opcion.descripcion);
+      console.log(Opcion.identificador);
     })
 
-    const lista = createList(Menu);
-  
+    // Add the HTML markup to the page
     document.getElementById('Menuhtml').innerHTML = lista;
   })
-  .catch(error => console.error(error))
+  .catch(error => console.error(error));
 
+// Create the HTML markup for the list
+function CrearMenu(data) {
+  // Your code here
+}
 
+/*let lista = document.getElementById("Menuhtml");
+ 
+var ul = document.createElement('ul');
+document.getElementById('Menuhtml').appendChild(ul);
 
+Menu.forEach((Father) => {
+  let li = document.createElement("li");
+  li.innerText = Father.descripcion;
+  ul.appendChild(li);
 
+  
 
-    /*let lista = document.getElementById("Menuhtml");
-    
-    var ul = document.createElement('ul');
-		document.getElementById('Menuhtml').appendChild(ul);
-
-    Menu.forEach((Father) => {
-      let li = document.createElement("li");
-      li.innerText = Father.descripcion;
-      ul.appendChild(li);
+  Menu.forEach(Hijo => {
+    if (Father.identificador == Hijo.padre) {
+      console.log(Father.descripcion + " es el padre de " + Hijo.descripcion);
 
       
-
-      Menu.forEach(Hijo => {
-        if (Father.identificador == Hijo.padre) {
-          console.log(Father.descripcion + " es el padre de " + Hijo.descripcion);
-
-          
-        }
-      });
-  })
-  .catch(error => console.error(error))
+    }
+  });
+})
+.catch(error => console.error(error))
 
 /*console.log(Father.padre);
-        console.log(Father.descripcion);
-        console.log(Father.identificador);*/
+    console.log(Father.descripcion);
+    console.log(Father.identificador);*/
