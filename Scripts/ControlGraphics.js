ff6bfc9af087de49378755a31b767ea82388390a
joyller <106361452+joyller@users.noreply.github.com>
2023-05-02 22:08:07 -0400

    losing control

---
+++ Scripts/ControlGraphics.js
@@ -0,0 +1,17 @@
+async function fetchData() {
+    const url = 'https://joyller.github.io/datafile/Ventas.json';
+    const response = await fetch(url);
+
+    const datapoint = await response.json();
+
+    console.log(datapoint);
+    return datapoint;
+};
+
+fetchData().then(datapoint => {
+    const ListProducto = datapoint.Reporte[0].tipoProducto.map(
+        function(index){
+            return index.producto;
+        })
+        console.log(ListProducto);
+});
