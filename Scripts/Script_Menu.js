7015bb3e49e8fe6e1366adfd093319f0ba620ebb
joyller <106361452+joyller@users.noreply.github.com>
2023-05-08 21:37:52 -0400

    45465

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -2,6 +2,37 @@ fetch('https://joyller.github.io/datafile/Menu.json')
   .then(response => response.json())
   .then(DatosMenu => {
 
+    const menuItems = new Map();
+    for (const item of data) {
+      menuItems.set(item.ID, item);
+    }
+
+    const menuElement = document.getElementById('menu');
+    for (const item of data) {
+      if (!item.father) {
+        const liElement = document.createElement('li');
+        const aElement = document.createElement('a');
+        aElement.textContent = item.description;
+        liElement.appendChild(aElement);
+        menuElement.appendChild(liElement);
+      } else {
+        const liElement = document.createElement('li');
+        const aElement = document.createElement('a');
+        aElement.textContent = item.description;
+        liElement.appendChild(aElement);
+        const parent = menuItems.get(item.father);
+        const parentLiElement = menuElement.querySelector(`[data-id="${parent.ID}"]`);
+        if (parentLiElement) {
+          const ulElement = parentLiElement.querySelector('ul');
+          if (!ulElement) {
+            const newUlElement = document.createElement('ul');
+            parentLiElement.appendChild(newUlElement);
+            ulElement = newUlElement;
+          }
+          ulElement.appendChild(liElement);
+        }
+      }
+    }
     DatosMenu.forEach(Opcion => { // ver los datos
       console.log(Opcion.identificador);
       console.log(Opcion.descripcion);
