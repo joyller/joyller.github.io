2ae4d60b900375ee2631c860d304f493fcee68ed
joyller <106361452+joyller@users.noreply.github.com>
2023-05-08 22:15:06 -0400

    asd

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -1,22 +1,26 @@
-fetch('https://joyller.github.io/datafile/Menu.json')
-.then(response => response.json())
-.then(data => {
-  const menuItems = new Map();
-  for (const item of data) {
-    menuItems.set(item.identificador, item);
-  }
+const menuUrl = 'menu.json';
 
-  const menuElement = document.getElementById('MenuNav');
-  for (const item of data) {
-    if (!item.padre) {
-      const liElement = document.createElement('li');
-      const aElement = document.createElement('a');
-      aElement.textContent = item.descripcion;
-      liElement.appendChild(aElement);
-      menuElement.appendChild(liElement);
-    } 
-  }
-})
-.catch(error => {
-  console.error(error);
-});
+function createMenu(parentId, menu) {
+  const menuItems = menu.filter(item => item.padre === parentId);
+  if (!menuItems.length) return null;
+  const ulElement = document.createElement('ul');
+  menuItems.forEach(item => {
+    const liElement = document.createElement('li');
+    const aElement = document.createElement('a');
+    aElement.textContent = item.descripcion;
+    liElement.appendChild(aElement);
+    const subMenu = createMenu(item.identificador, menu);
+    if (subMenu) liElement.appendChild(subMenu);
+    ulElement.appendChild(liElement);
+  });
+  return ulElement;
+}
+
+fetch('https://joyller.github.io/datafile/Menu.json')  
+.then(response => response.json())
+  .then(data => {
+    const menuElement = document.getElementById('menu');
+    const menu = createMenu(null, data);
+    menuElement.appendChild(menu);
+  })
+  .catch(error => console.error(error));
