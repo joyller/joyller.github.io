4829c6f29464c5142e46dbeb63931011a5a108dc
joyller <106361452+joyller@users.noreply.github.com>
2023-05-08 22:20:39 -0400

    dsdfa

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -1,26 +1,16 @@
-const menuUrl = 'menu.json';
-
-function createMenu(parentId, menu) {
-  const menuItems = menu.filter(item => item.padre === parentId);
-  if (!menuItems.length) return null;
-  const ulElement = document.createElement('ul');
-  menuItems.forEach(item => {
-    const liElement = document.createElement('li');
-    const aElement = document.createElement('a');
-    aElement.textContent = item.descripcion;
-    liElement.appendChild(aElement);
-    const subMenu = createMenu(item.identificador, menu);
-    if (subMenu) liElement.appendChild(subMenu);
-    ulElement.appendChild(liElement);
-  });
-  return ulElement;
-}
-
-fetch('https://joyller.github.io/datafile/Menu.json')  
-.then(response => response.json())
-  .then(data => {
-    const menuElement = document.getElementById('MenuNav');
-    const menu = createMenu(null, data);
-    menuElement.appendChild(menu);
+fetch('https://joyller.github.io/datafile/Menu.json')
+  .then(response => response.json())
+  .then(DatosMenu => {
+    DatosMenu.forEach(Opcion => { // ver los datos
+      console.log(Opcion.identificador);
+      console.log(Opcion.descripcion);
+      console.log(Opcion.padre);
+    })
   })
   .catch(error => console.error(error));
+
+function creadorMenu(MenuData){
+  MenuData.forEach(caso => {
+    console.log(caso.descripcion[caso]);
+  })
+}
