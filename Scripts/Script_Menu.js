344d482b496717fc8d6cf72566f14d5703624801
joyller <106361452+joyller@users.noreply.github.com>
2023-05-08 22:41:09 -0400

    dg

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -1,6 +1,6 @@
 fetch('https://joyller.github.io/datafile/Menu.json')
   .then(response => response.json())
-  .then(as => {
+  .then(DatosMenu => {
     DatosMenu.forEach(Opcion => { // ver los datos
       console.log(Opcion.identificador);
       console.log(Opcion.descripcion);
@@ -9,8 +9,20 @@ fetch('https://joyller.github.io/datafile/Menu.json')
   })
   .catch(error => console.error(error));
 
-function creadorMenu(MenuData){
-  MenuData.forEach(caso => {
-    console.log(caso.descripcion[caso]);
-  })
-}
+  let contenedor = document.createElement("ul");
+  
+  function obj(DatosMenu, contenedor){
+    DatosMenu.forEach(caso =>{
+      objeto = DatosMenu[caso];
+      var li = document.createElement("li");
+      if(objeto.padre != null){
+        var a = document.createElement("a");
+        a.innerHTML = objeto.descripcion;
+        a.href = objeto.url;
+        li.appendChild(a);
+        contenedor.appendChild(li)
+      }
+    })
+
+    return contenedor;
+  }
