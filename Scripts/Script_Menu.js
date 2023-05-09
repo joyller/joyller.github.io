5c3090a1bc8b865da69d9b9126a22f520bfbbd23
joyller <106361452+joyller@users.noreply.github.com>
2023-05-08 21:46:55 -0400

    sad

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -1,48 +1,38 @@
 fetch('https://joyller.github.io/datafile/Menu.json')
-  .then(response => response.json())
-  .then(DatosMenu => {
+.then(response => response.json())
+.then(data => {
+  const menuItems = new Map();
+  for (const item of data) {
+    menuItems.set(item.identificador, item);
+  }
 
-    const menuItems = new Map();
-    for (const item of DatosMenu) {
-      menuItems.set(item.ID, item);
-    }
-
-    const menuElement = document.getElementById('menu');
-    for (const item of DatosMenu) {
-      if (!item.father) {
-        const liElement = document.createElement('li');
-        const aElement = document.createElement('a');
-        aElement.textContent = item.description;
-        liElement.appendChild(aElement);
-        menuElement.appendChild(liElement);
-      } else {
-        const liElement = document.createElement('li');
-        const aElement = document.createElement('a');
-        aElement.textContent = item.description;
-        liElement.appendChild(aElement);
-        const parent = menuItems.get(item.father);
-        const parentLiElement = menuElement.querySelector(`[data-id="${parent.ID}"]`);
-        if (parentLiElement) {
-          const ulElement = parentLiElement.querySelector('ul');
-          if (!ulElement) {
-            const newUlElement = document.createElement('ul');
-            parentLiElement.appendChild(newUlElement);
-            ulElement = newUlElement;
-          }
-          ulElement.appendChild(liElement);
+  const menuElement = document.getElementById('menu');
+  for (const item of data) {
+    if (!item.padre) {
+      const liElement = document.createElement('li');
+      const aElement = document.createElement('a');
+      aElement.textContent = item.descripcion;
+      liElement.appendChild(aElement);
+      menuElement.appendChild(liElement);
+    } else {
+      const liElement = document.createElement('li');
+      const aElement = document.createElement('a');
+      aElement.textContent = item.descripcion;
+      liElement.appendChild(aElement);
+      const parent = menuItems.get(item.padre);
+      const parentLiElement = menuElement.querySelector(`[data-id="${parent.identificador}"]`);
+      if (parentLiElement) {
+        const ulElement = parentLiElement.querySelector('ul');
+        if (!ulElement) {
+          const newUlElement = document.createElement('ul');
+          parentLiElement.appendChild(newUlElement);
+          ulElement = newUlElement;
         }
+        ulElement.appendChild(liElement);
       }
     }
-    DatosMenu.forEach(Opcion => { // ver los datos
-      console.log(Opcion.identificador);
-      console.log(Opcion.descripcion);
-      console.log(Opcion.padre);
-    })
-  })
-  .catch(error => console.error(error));
-
-function creadorMenu(MenuData){
-  MenuData.forEach(caso => {
-    console.log(caso.descripcion[caso]);
-  })
-}
+  }
+})
+.catch(error => {
+  console.error(error);
+});
