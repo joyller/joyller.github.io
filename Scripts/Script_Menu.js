4b01ed55ed9e80148f9cbbc26cd76bc7e52e0ee6
joyller <106361452+joyller@users.noreply.github.com>
2023-05-08 20:48:51 -0400

    ???

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -1,11 +1,11 @@
 fetch(`https://joyller.github.io/datafile/Menu.json`)
   .then(response => response.json())
-  .then(DataMenu => {
+  .then(DatosMenu => {
 
-    const contMenu = document.getElementById("Menuhtml");
-    contMenu.appendChild(creadorMenu(DataMenu));
+    const contMenu = document.getElementById("Menuhtml"); // recibir el nav del menu
+    contMenu.appendChild(creadorMenu(DatosMenu));
 
-    DataMenu.forEach(Opcion => {
+    DatosMenu.forEach(Opcion => { // ver los datos
       console.log(Opcion.identificador);
       console.log(Opcion.descripcion);
       console.log(Opcion.padre);
@@ -14,5 +14,12 @@ fetch(`https://joyller.github.io/datafile/Menu.json`)
   .catch(error => console.error(error));
 
 function creadorMenu(MenuData){
-  
+  const pad = document.createElement("ul");
+  MenuData.forEach(caso => {
+    const lista = document.createElement("li");
+    const cont = document.createElement("ul");
+    const parrafo = document.createElement("p");
+
+    parrafo.appendChild(document.createTextNode(MenuData[caso].descripcion));
+  })
 }
