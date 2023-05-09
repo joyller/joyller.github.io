4d6d92b94e561aa37c24734ebb21c8bd9ca5f76b
joyller <106361452+joyller@users.noreply.github.com>
2023-05-08 22:21:41 -0400

    sfdfgjfg

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -1,6 +1,6 @@
 fetch('https://joyller.github.io/datafile/Menu.json')
   .then(response => response.json())
-  .then(DatosMenu => {
+  .then(as => {
     DatosMenu.forEach(Opcion => { // ver los datos
       console.log(Opcion.identificador);
       console.log(Opcion.descripcion);
