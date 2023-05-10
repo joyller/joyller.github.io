b1342b1cdf0a1bd307c2b337b8c6bbddc99f2cdf
joyller <106361452+joyller@users.noreply.github.com>
2023-05-09 21:25:49 -0400

    cxbvcx

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -8,27 +8,3 @@ fetch('https://joyller.github.io/datafile/Menu.json')
     })
   })
   .catch(error => console.error(error));
-
-  let contenedor = document.createElement("ul");
-  
-  function crearMenu(DatosMenu, contenedor)
-  {
-    for(x = 0 ; x < DatosMenu.length; x++){
-      var objeto = DatosMenu[x];
-      var li = document.createElement("li");
-      if(objeto.padre === null){
-        var a = document.createElement("a");
-        a.innerHTML = objeto.descripcion;
-        a.href = objeto.url;
-        li.appendChild(a);
-        contenedor.appendChild(li)
-    }
-  }
-
-    return contenedor;
-  }
-
-  contenedor = crearMenu(DatosMenu, contenedor);
-
-let lisNaveg = document.querySelector("lista");
-lisNaveg.appendChild(contenedor);
