e50fc80bd648b52720737e9575afc0d2b2832608
joyller <106361452+joyller@users.noreply.github.com>
2023-05-09 21:42:28 -0400

    vczx

--- Scripts/Script_Menu.js
+++ Scripts/Script_Menu.js
@@ -5,13 +5,23 @@ fetch('https://joyller.github.io/datafile/Menu.json')
     var contenedor = document.getElementById("Menu");
     var lista = document.createElement("ul");
 
+    
     DatosMenu.forEach(Opcion => { // ver los datos
+
+      lista.textContent = Opcion.descripcion;
+
+      Menu.appendChild(lista);
+
+      lista = document.createElement('li');
+
       console.log(Opcion.identificador);
       console.log(Opcion.descripcion);
       console.log(Opcion.padre);
 
       lista.createElement("li").Opcion.descripcion;
 
+      const menu = document.querySelector('#Menu');
+      Menu.appendChild(li);
     })
   })
   .catch(error => console.error(error));
