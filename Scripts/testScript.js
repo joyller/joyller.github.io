923139bc60ea7222c26b1b1ab8a002c41a5e51a9
joyller <106361452+joyller@users.noreply.github.com>
2023-05-13 23:41:41 -0400

    cxv

--- Scripts/testScript.js
+++ Scripts/testScript.js
@@ -1,11 +1,10 @@
 function ajax(){
     const http = new XMLHttpRequest();
-    const url = 'https://joyller.github.io/';
+    const url = 'https://joyller.github.io';
 
     http.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
             console.log(this.responseText);
-
         }
     }
 
@@ -13,6 +12,6 @@ function ajax(){
     http.send();
 }
 
-document.getElementById("llevar").addEventListener("click",function(){
+document.getElementById("llevar").addEventListener("click", function(){
     ajax();
 });
