0c71901594d77b404c3b12a056f718668b8a9ed5
joyller <106361452+joyller@users.noreply.github.com>
2023-05-13 23:36:15 -0400

    sad

---
+++ Scripts/testScript.js
@@ -0,0 +1,18 @@
+function ajax(){
+    const http = new XMLHttpRequest();
+    const url = 'https://joyller.github.io/';
+
+    http.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            console.log(this.responseText);
+
+        }
+    }
+
+    http.open("GET", url);
+    http.send();
+}
+
+document.getElementById("llevar").addEventListener("click",function(){
+    ajax();
+});
