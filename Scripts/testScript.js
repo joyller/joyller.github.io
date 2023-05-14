6777ed05869f142f44c35d95ee6a2e4742fc67b1
joyller <106361452+joyller@users.noreply.github.com>
2023-05-13 23:49:48 -0400

    v

--- Scripts/testScript.js
+++ Scripts/testScript.js
@@ -1,6 +1,6 @@
 function ajax(){
     const http = new XMLHttpRequest();
-    const url = 'https://joyller.github.io';
+    const url = 'index.html';
 
     http.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
